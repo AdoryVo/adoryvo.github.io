6156da19ccef3918121be29f45cd948ca5a53844
AdoryVo <adoryvo@gmail.com>
2019-08-27 19:00:33 -0700

    Indents, music

--- projects/js/pace-calculator.js
+++ projects/js/pace-calculator.js
@@ -1,22 +1,22 @@
 function calculate() {
-  var form = document.getElementById("frm1");
-  var dist = form.elements["dist"].value;
-  var mins = form.elements["mins"].value;
-  var secs = parseInt(form.elements["secs"].value);
+    var form = document.getElementById("frm1");
+    var dist = form.elements["dist"].value;
+    var mins = form.elements["mins"].value;
+    var secs = parseInt(form.elements["secs"].value);
 
-  var pace = (mins * 60 + secs) / dist
-  var left = Math.floor(pace / 60);
-  var right = (pace % 60).toFixed(2);
-  if (right < 10) {
-    right = "0" + right;
-  }
+    var pace = (mins * 60 + secs) / dist
+    var left = Math.floor(pace / 60);
+    var right = (pace % 60).toFixed(2);
+    if (right < 10) {
+        right = "0" + right;
+    }
 
-  if (secs < 10) {
-    secs = "0" + secs;
-  }
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
 
-  document.getElementById("dist").innerHTML = dist;
-  document.getElementById("mins").innerHTML = mins;
-  document.getElementById("secs").innerHTML = secs;
-  document.getElementById("pace").innerHTML = left + ":" + right + " min/mile";
+    document.getElementById("dist").innerHTML = dist;
+    document.getElementById("mins").innerHTML = mins;
+    document.getElementById("secs").innerHTML = secs;
+    document.getElementById("pace").innerHTML = left + ":" + right + " min/mile";
 }
--- projects/js/result-time-calculator.js
+++ projects/js/result-time-calculator.js
@@ -1,22 +1,22 @@
 function calculate() {
-  var form = document.getElementById("frm1");
-  var dist = form.elements["dist"].value;
-  var mins = form.elements["mins"].value;
-  var secs = parseInt(form.elements["secs"].value);
+    var form = document.getElementById("frm1");
+    var dist = form.elements["dist"].value;
+    var mins = form.elements["mins"].value;
+    var secs = parseInt(form.elements["secs"].value);
 
-  var total = (mins * 60 + secs) * dist;
-  var left = Math.floor(total / 60);
-  var right = total % 60;
-  if (right < 10) {
-    right = "0" + right;
-  }
+    var total = (mins * 60 + secs) * dist;
+    var left = Math.floor(total / 60);
+    var right = total % 60;
+    if (right < 10) {
+        right = "0" + right;
+    }
 
-  if (secs < 10) {
-    secs = "0" + secs;
-  }
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
 
-  document.getElementById("dist").innerHTML = dist;
-  document.getElementById("mins").innerHTML = mins;
-  document.getElementById("secs").innerHTML = secs;
-  document.getElementById("result").innerHTML = left + ":" + right + " min";
+    document.getElementById("dist").innerHTML = dist;
+    document.getElementById("mins").innerHTML = mins;
+    document.getElementById("secs").innerHTML = secs;
+    document.getElementById("result").innerHTML = left + ":" + right + " min";
 }
