ae6a0df1bae53d593c2f24c81ce7d2fdd155af5c
Adory Vo <adoryvo@gmail.com>
2019-12-11 19:00:16 -0800

    code cleanup and slight visual changes

--- projects/js/easyCheck.js
+++ projects/js/easyCheck.js
@@ -1,3 +1,7 @@
+var payer_form = document.getElementById("add_form");
+var add_button = document.getElementById("add");
+var payer_table = document.getElementById("payers");
+
 function moneyConv(money) {
     return parseFloat(money).toFixed(2);
 }
@@ -22,5 +26,28 @@ function calcResult() {
 }
 
 function addPayer() {
+    let payer = document.createElement("tr");
     
+    let num = payer_form.children[0].cloneNode(true);
+    num.textContent = parseInt(num.textContent) + 1;
+    payer.appendChild(num);
+    
+    //console.log(num.textContent);
+    for (let i = 1; i < 5; i++) {
+        let col_value = payer_form.children[i].children[0].value;
+        console.log(col_value.children);
+        let col = document.createElement("td");
+        col.textContent = col_value;
+        
+        payer.appendChild(col);
+    }
+    
+    let rmv_button = document.createElement("td");
+    rmv_button.classList = "align-middle";
+    rmv_button.innerHTML = "<button type='button' id='rmv' class='bg-danger border-0 rounded-circle'><i class='fas fa-trash text-white'></i></button>";
+    payer.appendChild(rmv_button);
+    
+    payer_table.appendChild(payer);
 }
+
+add_button.addEventListener("click", addPayer);
