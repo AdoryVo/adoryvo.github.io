148f4f7b4dd4f54f5a3ca83893d0afa9ed85fd07
Adory Vo <adoryvo.business@gmail.com>
2020-08-24 18:28:28 -0700

    Update projects home

--- update-packages.js
+++ update-packages.js
@@ -5,7 +5,7 @@ fs.copy('node_modules/bootstrap/dist/css/bootstrap.min.css.map', 'dependencies/b
 fs.copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'dependencies/fontawesome.min.css');
 fs.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'webfonts');
 
-fs.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
-fs.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
-fs.copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'public/js/popper.min.js');
-fs.copy('node_modules/@popperjs/core/dist/umd/popper.min.js.map', 'public/js/popper.min.js.map');
+fs.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'dependencies/bootstrap.min.js');
+fs.copy('node_modules/jquery/dist/jquery.min.js', 'dependencies/jquery.min.js');
+fs.copy('node_modules/@popperjs/core/dist/umd/popper.min.js', 'dependencies/popper.min.js');
+fs.copy('node_modules/@popperjs/core/dist/umd/popper.min.js.map', 'dependencies/popper.min.js.map');
