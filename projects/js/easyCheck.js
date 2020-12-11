6058bea337307383b903abb3aaa559502e7bcd50
Adory Vo <adoryvo.business@gmail.com>
2020-12-10 18:57:07 -0800

    EasyCheck Project -  Subtotal Share Calculator

--- projects/js/easyCheck.js
+++ projects/js/easyCheck.js
@@ -11,6 +11,12 @@ function calcResult() {
     let tip_percent = form.elements["tip"].value;
     let num_payers = form.elements["num_payers"].value;
 
+    console.log(typeof num_payers);
+
+    if (!num_payers) {
+        return;
+    }
+
     let tip = moneyConv(subtotal * tip_percent / 100);
     let total = moneyConv(parseFloat(subtotal) + parseFloat(tip));
 
@@ -22,6 +28,44 @@ function calcResult() {
     document.getElementById("avg_subtotal").innerHTML = moneyConv(subtotal / num_payers);
     document.getElementById("avg_tip").innerHTML = moneyConv(tip / num_payers);
     document.getElementById("avg_total").innerHTML = moneyConv(total / num_payers);
+}
+
+function calcSubShare() {
+    let form = document.getElementById("subShareForm");
+    let indSubtotal = moneyConv(form.elements["subIndSubtotal"].value);
+    let subtotal = moneyConv(form.elements["subSubtotal"].value);
+    let tax = moneyConv(form.elements["subTax"].value);
+
+    if (isNaN(Number(indSubtotal))) {
+        return;
+    }
+
+    if (isNaN(Number(subtotal))) {
+        subtotal = 1;
+    }
+
+    if (isNaN(Number(tax))) {
+        tax = 0;
+    }
+
+    let taxShare = (indSubtotal / subtotal) * tax;
+
+    let subtotalShare = taxShare + parseFloat(indSubtotal);
+
+    document.getElementById("subTaxResult").innerHTML = moneyConv(taxShare);
+    document.getElementById("subSubShareResult").innerHTML = moneyConv(subtotalShare);
+}
+
+function updateTotal() {
+    let form = document.getElementById("payerForm");
+    let subtotal = moneyConv(form.elements["add_subtotal"].value);
+    let tip = moneyConv(form.elements["add_tip"].value);
+
+    if (isNaN(Number(subtotal)) || isNaN(Number(tip))) {
+        return;
+    }
+
+    document.getElementById("totalResult").innerHTML = moneyConv(parseFloat(subtotal) + parseFloat(tip));
 }
 
 function addPayer() {
@@ -39,7 +83,7 @@ function addPayer() {
         let col = document.createElement("td");
         let col_value = payer_form.children[i].children[0].children[0].value;
 
-        col_value = parseInt(col_value).toFixed(2);
+        col_value = moneyConv(col_value);
 
         col.textContent = "$" + col_value.toString();
 
@@ -59,15 +103,13 @@ function addPayer() {
 function removePayer(rowNum) {
     payer_table.removeChild(payer_table.children[rowNum - 1]);
 
-    for (let i = rowNum - 1; i < payer_table.children.length; i++)
-    {
+    for (let i = rowNum - 1; i < payer_table.children.length; i++) {
         let row = payer_table.children[i];
         let oldRowNum = row.children[0].textContent;
         row.children[0].textContent = (parseInt(oldRowNum) - 1).toString();
 
-        if (i !== payer_table.children.length - 1)
-        {
-            row.children[5].children[0].setAttribute('onclick',`removePayer(${parseInt(oldRowNum) - 1})`);
+        if (i !== payer_table.children.length - 1) {
+            row.children[5].children[0].setAttribute('onclick', `removePayer(${parseInt(oldRowNum) - 1})`);
         }
     }
 }
