33fb7c23e0668f40bf28bc83f6f30897cd78b8d7
Adory Vo <adoryvo.business@gmail.com>
2021-01-27 06:00:32 -0800

    Profile image resize quickfix and jQuery document ready quickfix

--- dist/scripts/footer-init.js
+++ dist/scripts/footer-init.js
@@ -1,6 +1,8 @@
-const footer = $('footer');
-if ($('body').get(0).scrollHeight <= window.innerHeight) {
-    footer.toggleClass('relative');
-    footer.addClass('absolute');
-    footer.addClass('bottom-0');
-}
+$(document).ready(() => {
+    const footer = $('footer');
+    if ($('body').get(0).scrollHeight <= window.innerHeight) {
+        footer.toggleClass('relative');
+        footer.addClass('absolute');
+        footer.addClass('bottom-0');
+    }
+});
