0dc52b6516b199df3a268e56e40611a7aee304eb
Adory Vo <adoryvo.business@gmail.com>
2021-01-27 06:57:34 -0800

    Responsiveness quickfix and music page update

--- dist/scripts/footer-init.js
+++ dist/scripts/footer-init.js
@@ -1,8 +1,22 @@
 $(document).ready(() => {
     const footer = $('footer');
-    if ($('body').get(0).scrollHeight <= window.innerHeight) {
-        footer.toggleClass('relative');
-        footer.addClass('absolute');
-        footer.addClass('bottom-0');
+
+    function toggleFooterPosition() {
+        if ($('body').get(0).scrollHeight <= window.innerHeight) {
+            footer.removeClass('relative');
+            footer.addClass('absolute');
+            footer.addClass('bottom-0');
+        }
+        else {
+            footer.addClass('relative');
+            footer.removeClass('absolute');
+            footer.removeClass('bottom-0');
+        }
     }
+
+    toggleFooterPosition();
+
+    $(window).resize(() => {
+        toggleFooterPosition();
+    });
 });
